5de45770074a843407888bd07281b7190ed0570e
Jurgen Roos <roos.jurgen@gmail.com>
2021-03-31 10:19:24 +0200

    Initial Spring Project

---
+++ src/main/java/za/co/dariel/deap/endpointsecurity/EndpointsecurityApplication.java
@@ -0,0 +1,13 @@
+package za.co.dariel.deap.endpointsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndpointsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndpointsecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/dariel/deap/endpointsecurity/EndpointsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.dariel.deap.endpointsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndpointsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
