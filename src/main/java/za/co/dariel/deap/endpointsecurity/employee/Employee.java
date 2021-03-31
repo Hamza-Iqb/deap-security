abafeda1c9beaa717fb98c51c5d9c1bd579ecb99
Hamza-Iqb <81677259+Hamza-Iqb@users.noreply.github.com>
2021-03-31 13:29:54 +0200

    Set 'email' to be character primary key in table

--- src/main/java/za/co/dariel/deap/endpointsecurity/employee/Employee.java
+++ src/main/java/za/co/dariel/deap/endpointsecurity/employee/Employee.java
@@ -1,8 +1,6 @@
 package za.co.dariel.deap.endpointsecurity.employee;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Getter;
@@ -14,7 +12,6 @@ import lombok.Setter;
 public class Employee {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private String email;
 	private String firstName;
 	private String lastName;
