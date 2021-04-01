87591f81780cdf4f51e301312f1e679d27ea3160
Hamza-Iqb <81677259+Hamza-Iqb@users.noreply.github.com>
2021-04-01 11:48:22 +0200

    Implemented UserDetails for security

--- src/main/java/za/co/dariel/deap/endpointsecurity/employee/Employee.java
+++ src/main/java/za/co/dariel/deap/endpointsecurity/employee/Employee.java
@@ -1,7 +1,14 @@
 package za.co.dariel.deap.endpointsecurity.employee;
 
+import java.util.Collection;
+import java.util.Collections;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,15 +18,14 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @Entity
-public class Employee {
-	
+public class Employee implements UserDetails {
+
 	@Id
 	private String email;
 	private String firstName;
 	private String lastName;
 	private String password;
-	
-	
+
 	public Employee(String email, String firstName, String lastName, String password) {
 		super();
 		this.email = email;
@@ -27,7 +33,44 @@ public class Employee {
 		this.lastName = lastName;
 		this.password = password;
 	}
-	
-	
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+//		SimpleGrantedAuthority authority = new SimpleGrantedAuthority(this.firstName);
+//		
+//		return Collections.singletonList(authority);
+
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 
 }
