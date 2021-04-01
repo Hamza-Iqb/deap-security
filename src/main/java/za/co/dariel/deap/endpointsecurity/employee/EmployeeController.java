169adcd86db23a393bdaa2541c96a74a20ec29a1
hannovdw <hannovdw1998@gmail.com>
2021-04-01 10:36:27 +0200

    Added CORS policy

--- src/main/java/za/co/dariel/deap/endpointsecurity/employee/EmployeeController.java
+++ src/main/java/za/co/dariel/deap/endpointsecurity/employee/EmployeeController.java
@@ -2,31 +2,29 @@ package za.co.dariel.deap.endpointsecurity.employee;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.AllArgsConstructor;
 
+@CrossOrigin(origins = "http://localhost:4200" , maxAge = 3600)// allow cors for front end
 @RestController
 @AllArgsConstructor
 @RequestMapping("employee")
 public class EmployeeController {
 	
 	private final EmployeeService employeeService;
-	
+
 	@PostMapping("/registration")
 	public String registerNewEmployee(@RequestBody Employee employee) {
 		employeeService.addEmployee(employee);
 		return "Employee successfully added";
 	}
-	
-	
+
+	@GetMapping("/test")
+	public String testApi(){
+		return "API IS WORKING!!!";
+	}
+
 	@GetMapping("/get")
 	public List<Employee> showAllEmployees(){
 		
