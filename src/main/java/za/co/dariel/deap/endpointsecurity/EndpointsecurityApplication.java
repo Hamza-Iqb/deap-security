fd7705395b853d8df7ccfd6847ea8990c59d409a
Hamza-Iqb <81677259+Hamza-Iqb@users.noreply.github.com>
2021-04-06 11:40:16 +0200

    configure ModelMapper class as Spring bean to inject in restcontroller class

--- src/main/java/za/co/dariel/deap/endpointsecurity/EndpointsecurityApplication.java
+++ src/main/java/za/co/dariel/deap/endpointsecurity/EndpointsecurityApplication.java
@@ -1,10 +1,18 @@
 package za.co.dariel.deap.endpointsecurity;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EndpointsecurityApplication {
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(EndpointsecurityApplication.class, args);
