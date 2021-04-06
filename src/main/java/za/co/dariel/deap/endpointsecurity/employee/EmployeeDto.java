56bb124d1572b015c06088b504d7b9c244b47fa5
Hamza-Iqb <81677259+Hamza-Iqb@users.noreply.github.com>
2021-04-06 11:38:31 +0200

    Create EmployeeDto.java

---
+++ src/main/java/za/co/dariel/deap/endpointsecurity/employee/EmployeeDto.java
@@ -0,0 +1,26 @@
+package za.co.dariel.deap.endpointsecurity.employee;
+
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class EmployeeDto {
+
+	@Id
+	private String email;
+	private String firstName;
+	private String lastName;
+	private String password;
+
+	public EmployeeDto(String email, String firstName, String lastName, String password) {
+			super();
+			this.email = email;
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.password = password;
+		}
+
+}
