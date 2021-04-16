b8ba21a6fbb8b656b2520709d31ced8e6b20128c
Hamza-Iqb <81677259+Hamza-Iqb@users.noreply.github.com>
2021-04-16 13:59:18 +0200

    Fixing code smells in EmployeeEntity.java

--- src/main/java/za/co/dariel/deap/endpointsecurity/entities/EmployeeEntity.java
+++ src/main/java/za/co/dariel/deap/endpointsecurity/entities/EmployeeEntity.java
@@ -36,40 +36,37 @@ public class EmployeeEntity implements UserDetails {
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-//		SimpleGrantedAuthority authority = new SimpleGrantedAuthority(this.firstName);
-//		
-//		return Collections.singletonList(authority);
 
-		return null;
+		return Collections.emptyList();
 	}
 
 	@Override
 	public String getUsername() {
-		// TODO Auto-generated method stub
+
 		return email;
 	}
 
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
+
 		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
+
 		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
+
 		return true;
 	}
 
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
+
 		return true;
 	}
 
