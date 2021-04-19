79c41b44aeb956e795d5f03825486c07eb4c0236
Hamza-Iqb <81677259+Hamza-Iqb@users.noreply.github.com>
2021-04-19 15:28:22 +0200

    Create RegularJsonConfigProviderFactory.java

---
+++ src/main/java/za/co/dariel/deap/endpointsecurity/security/config/RegularJsonConfigProviderFactory.java
@@ -0,0 +1,7 @@
+package za.co.dariel.deap.endpointsecurity.security.config;
+
+import org.keycloak.services.util.JsonConfigProviderFactory;
+
+public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
+
+}
