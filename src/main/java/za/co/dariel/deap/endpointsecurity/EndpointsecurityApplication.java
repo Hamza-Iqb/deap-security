26a2e8b441be8b51db76f9ebe4d8a3adb1d87203
Hamza-Iqb <81677259+Hamza-Iqb@users.noreply.github.com>
2021-04-19 15:27:33 +0200

    Update EndpointsecurityApplication.java

--- src/main/java/za/co/dariel/deap/endpointsecurity/EndpointsecurityApplication.java
+++ src/main/java/za/co/dariel/deap/endpointsecurity/EndpointsecurityApplication.java
@@ -1,18 +1,39 @@
 package za.co.dariel.deap.endpointsecurity;
 
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
+import za.co.dariel.deap.endpointsecurity.security.config.KeycloakServerProperties;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = LiquibaseAutoConfiguration.class)
+@EnableConfigurationProperties(KeycloakServerProperties.class)
 public class EndpointsecurityApplication {
+
+	private static final Logger LOG = LoggerFactory.getLogger(EndpointsecurityApplication.class);
 	
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
-	
+
+	@Bean
+	ApplicationListener<ApplicationReadyEvent> onApplicationReadyEventListener(
+			ServerProperties serverProperties, KeycloakServerProperties keycloakServerProperties) {
+		return (evt) -> {
+			Integer port = serverProperties.getPort();
+			String keycloakContextPath = keycloakServerProperties.getContextPath();
+			LOG.info("Embedded Keycloak started: http://localhost:{}{} to use keycloak",
+					port, keycloakContextPath);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EndpointsecurityApplication.class, args);
