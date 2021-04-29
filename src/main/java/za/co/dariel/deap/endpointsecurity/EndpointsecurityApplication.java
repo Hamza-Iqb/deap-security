210cd6969f0d3fda7b03b8b31f9992a6756806ec
Hamza-Iqb <81677259+Hamza-Iqb@users.noreply.github.com>
2021-04-29 10:39:41 +0200

    Update EndpointsecurityApplication.java

--- src/main/java/za/co/dariel/deap/endpointsecurity/EndpointsecurityApplication.java
+++ src/main/java/za/co/dariel/deap/endpointsecurity/EndpointsecurityApplication.java
@@ -2,8 +2,16 @@ package za.co.dariel.deap.endpointsecurity;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class})
 
 @SpringBootApplication
 public class EndpointsecurityApplication {
